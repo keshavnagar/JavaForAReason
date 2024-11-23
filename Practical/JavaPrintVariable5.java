8b6de3f7a39a7cd784bea0ad4c72be7aa0f2cf8a
Keshav Nagar <keshavnagar887@gmail.com>
2024-11-23 21:12:56 +0530

    add somthing like in file number 5 and file number 9

--- Practical/JavaPrintVariable5.java
+++ Practical/JavaPrintVariable5.java
@@ -1,5 +1,5 @@
 package Practical;
-public class JavaPrintVariable {
+public class JavaPrintVariable5 {
     public static void main(String[] args) {
         String name = "keshav";
         System.out.println("hello"+" "+name);
