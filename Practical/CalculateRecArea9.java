b1062235b954e102e44b1828e5ebea35c7597c3f
Keshav Nagar <keshavnagar887@gmail.com>
2024-11-23 22:15:07 +0530

    add new files

---
+++ Practical/CalculateRecArea9.java
@@ -0,0 +1,9 @@
+package Practical;
+public class CalculateRecArea9 {
+    public static void main(String [] args ){
+        int height = 78;
+        int width = 90;
+        int area = height * width;
+        System.out.println("area of rectangle " + area);
+    }
+}
---
+++ Practical/JavaVariable.java
@@ -0,0 +1,14 @@
+package Practical;
+public class JavaVariable {
+    public static void main(String[] args) {
+        String studentName = "Kamal Kumar";
+        int studentId = 89;
+        float studentFee = 89.89f;
+        char studentGrade = 'A';
+        System.out.println("Student Name is "+studentName);
+        System.out.println("Student ID is "+studentId);
+        System.out.println("Student Fee is "+studentFee);
+        System.out.println("StudentGrade is "+ studentGrade);
+   
+    }
+}
