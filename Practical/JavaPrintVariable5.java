e2fe65c2a8d7df2a23958bd84733580c8debe23e
Keshav Nagar <keshavnagar887@gmail.com>
2024-11-23 21:31:28 +0530

    add some feature in JavaPrintVariable5 file

--- Practical/JavaPrintVariable5.java
+++ Practical/JavaPrintVariable5.java
@@ -1,14 +1,26 @@
 package Practical;
+
 public class JavaPrintVariable5 {
     public static void main(String[] args) {
+
+        // to cmbine both text and variable , use the + character.
+
         String name = "keshav";
-        System.out.println("hello"+" "+name);
+        System.out.println("hello" + " " + name);
+
+        // we can also combine two variables like firstname and last name = full name ;
 
         String firstName = "Keshav";
         String LastName = "Nagar";
 
-        String fullName = firstName+LastName;
+        String fullName = firstName + LastName;
         System.out.println(fullName);
+
+        // For numeric values , the + operator works as a mathematical operator
+
+        int x = 89;
+        int y = 90;
+        System.out.println(x + y);
     }
-    
+
 }
