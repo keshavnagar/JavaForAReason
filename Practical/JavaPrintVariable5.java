78e3bc6f0635aed8dec4182d6a7694925ff7dca3
Keshav Nagar <keshavnagar887@gmail.com>
2024-11-23 21:20:28 +0530

    change in JavaPrintVariable5

--- Practical/JavaPrintVariable5.java
+++ Practical/JavaPrintVariable5.java
@@ -3,6 +3,12 @@ public class JavaPrintVariable5 {
     public static void main(String[] args) {
         String name = "keshav";
         System.out.println("hello"+" "+name);
+
+        String firstName = "Keshav";
+        String LastName = "Nagar";
+
+        String fullName = firstName+LastName;
+        System.out.println(fullName);
     }
     
 }
